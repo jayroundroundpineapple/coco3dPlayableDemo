80cf95d86c2dbd6659d4b32661165da2a3001ef2
xiangwei <chenxiangwei@toponegames.cn>
2025-10-31 19:26:58 +0800

    bug修复

--- assets/script/game/game.ts
+++ assets/script/game/game.ts
@@ -1,14 +1,7 @@
 
-/**
- * Copyright (c) 2019 Xiamen Yaji Software Co.Ltd. All rights reserved.
- * Created by daisy on 2019/06/25.
- */
-import { _decorator, Component, Node, instantiate, Prefab, view, Canvas, ResolutionPolicy, director, Widget } from "cc";
+import { _decorator, Component, Node, Prefab, view, Canvas, ResolutionPolicy, director, Widget } from "cc";
 import { Constants } from "../data/constants";
-import { Ball } from "./ball";
-import { BoardManager } from "./board-manager";
 import { CameraCtrl } from "./camera-ctrl";
-import { UIManager } from "./ui-manager";
 import { AudioManager } from "./audio-manager";
 import { PlayerAdSdk } from "../PlayerAdSdk";
 const { ccclass, property } = _decorator;
@@ -20,12 +13,8 @@ const { ccclass, property } = _decorator;
 export class Game extends Component {
     @property(Prefab)
     ballPref: Prefab = null!;
-    @property(BoardManager)
-    boardManager: BoardManager = null!;
     @property(CameraCtrl)
     cameraCtrl: CameraCtrl = null!;
-    @property(UIManager)
-    uiManager: UIManager = null!;
     @property(AudioManager)
     audioManager: AudioManager = null!;
     @property(Node)
@@ -35,35 +24,21 @@ export class Game extends Component {
      // There is no diamond in first board
     initFirstBoard = false;
 
-    get ball(){
-        return this._ball;
-    }
+    
 
     state = Constants.GAME_STATE.READY;
     score = 0;
     hasRevive = false;
-    _ball: Ball = null!;
     __preload () {
         Constants.game = this;
     }
 
     onLoad(){
         PlayerAdSdk.init()
-        if (!this.ballPref) {
-            console.log('There is no ball!!');
-            this.enabled = false;
-            return;
-        }
         this.resize()
-        const ball = instantiate(this.ballPref) as Node;
-        // @ts-ignore
-        ball.parent = this.node.parent;
-        this._ball = ball.getComponent(Ball)!;
     }
 
     start(){
-        this.node.on(Constants.GAME_EVENT.RESTART, this.gameStart, this);
-        this.node.on(Constants.GAME_EVENT.REVIVE, this.gameRevive, this);
         let that = this;
         /**屏幕旋转尺寸改变 */
         view.setResizeCallback(() => {
@@ -72,64 +47,7 @@ export class Game extends Component {
     }
 
     onDestroy() {
-        this.node.off(Constants.GAME_EVENT.RESTART, this.gameStart, this);
-        this.node.off(Constants.GAME_EVENT.REVIVE, this.gameRevive, this);
-    }
-
-    resetGame() {
-        this.state = Constants.GAME_STATE.READY;
-        this._ball.reset();
-        this.cameraCtrl.reset();
-        this.boardManager.reset();
-        // this.uiManager.showDialog(true);
-        this.uiManager.showFailUI()
-    }
-
-    gameStart(){
-        this.audioManager.playSound();
-        this.uiManager.showDialog(false);
-        this.state = Constants.GAME_STATE.PLAYING;
-        this.hasRevive = false;
-        this.score = 0;
-    }
-
-    gameDie(){
-        this.audioManager.playSound(false);
-        this.state = Constants.GAME_STATE.PAUSE;
-
-        if (!this.hasRevive) {
-            this.node.emit(Constants.GAME_EVENT.DYING, ()=>{
-                this.gameOver();
-            });
-        } else {
-            this.gameOver();
-        }
-    }
-    gameSuccess(){
-        this.audioManager.playSound(false)
-        this.state = Constants.GAME_STATE.OVER;
-        this.uiManager.showSuccessUI()
-    }
-
-    gameOver() {
-        this.state = Constants.GAME_STATE.OVER;
-        this.audioManager.playSound(false);
-        this.resetGame();
-    }
-
-    gameRevive(){
-        this.hasRevive = false;
-        this.state = Constants.GAME_STATE.READY;
-        this.ball.revive();
-        this.scheduleOnce(() => {
-            this.audioManager.playSound();
-            this.state = Constants.GAME_STATE.PLAYING;
-        }, 1);
-    }
-
-    addScore(score: number){
-        this.score += score;
-        this.node.emit(Constants.GAME_EVENT.ADDSCORE, this.score);
+       
     }
     private resize() {
         let winSize = view.getVisibleSize() //View.getCanvasSize()
@@ -145,8 +63,8 @@ export class Game extends Component {
         } else {
             view.setResolutionPolicy(ResolutionPolicy.FIXED_HEIGHT)
         }
-        director.getScene().getComponentsInChildren(Widget).forEach(function (t) {
-            t.updateAlignment()
-        });
+        // director.getScene().getComponentsInChildren(Widget).forEach(function (t) {
+        //     t.updateAlignment()
+        // });
     }
 }
